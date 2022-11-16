c41512758d3d58e43e7e38ac252f3e1d14d4eee3
aniketmankari <mankarianiket>
2022-11-16 08:47:36 +0530

    done

--- hello.java
+++ hello.java
@@ -9,6 +9,7 @@ System.out.println("Hello World! Welcome to jenkins.");
 System.out.println("Hello World!");
 System.out.println("Hello World!");
 System.out.println("REFLECTED CHANGES");
+System.out.println("new branch b1 is created and changes are pushed to remote ");
 
 
 
